c46ce0cf1b1a206d75ceb21c7bd9f8237db202f9
Andrew Wong <kkwong125@gmail.com>
2020-09-22 00:32:55 -0700

    Created logging on more methods.

--- Server/Logging.java
+++ Server/Logging.java
@@ -1,11 +1,26 @@
 package Server;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.ProceedingJoinPoint;
 
 @Aspect
 public class Logging {
+
+    @After("call(java.net.ServerSocket.new(..))")
+    public void afterServerSocket() {
+        System.out.println("Server listening on port " + DirServer.PORT);
+    }
+
+    @Before("execution(* Server.DirServerThread.run(..))")
+    public void beforeDirServerThreadRun() {
+        System.out.println("Thread " + Thread.currentThread().getId() + ": Start DirServerThread.run().");
+    }
+
+    @After("execution(* Server.DirServerThread.run(..))")
+    public void afterDirServerThreadRun() {
+        System.out.println("Thread " + Thread.currentThread().getId() + ": Finish DirServerThread.run().");
+    }
+
     @Before("call(void Server.Servlet.Servlet.doGet(..))")
     public void beforeDoGetCall() {
         System.out.println("Thread " + Thread.currentThread().getId() + ": Start DirServlet.doGet().");
@@ -26,14 +41,23 @@ public class Logging {
         System.out.println("Thread " + Thread.currentThread().getId() + ": Finish HttpRequest.parseRequest().");
     }
 
-    @Before("call(String Server.Servlet.HttpResponse.composeHttpResponse(..))")
+    @Before("call(String Server.Servlet.HttpResponse.getComposedHttpResponse())")
     public void beforeComposeHttpResponseCall() {
-        System.out.println("Thread " + Thread.currentThread().getId() + ": Start HttpResponse.composeHttpResponse().");
+        System.out.println("Thread " + Thread.currentThread().getId() + ": Start HttpResponse.getComposedHttpResponse().");
     }
 
-    @After("call(String Server.Servlet.HttpResponse.composeHttpResponse(..))")
+    @After("call(String Server.Servlet.HttpResponse.getComposedHttpResponse())")
     public void afterComposeHttpResponseCall() {
-        System.out.println("Thread " + Thread.currentThread().getId() + ": Finish HttpResponse.composeHttpResponse().");
+        System.out.println("Thread " + Thread.currentThread().getId() + ": Finish HttpResponse.getComposedHttpResponse().");
     }
 
+    @Before("call(* Server.Servlet.HttpResponse.getBodyWriter())")
+    public void beforeGetBodyWriterCall() {
+        System.out.println("Thread " + Thread.currentThread().getId() + ": Start HttpResponse.getBodyWriter().");
+    }
+
+    @After("call(* Server.Servlet.HttpResponse.getBodyWriter())")
+    public void afterGetBodyWriterCall() {
+        System.out.println("Thread " + Thread.currentThread().getId() + ": Finish HttpResponse.getBodyWriter().");
+    }
 }
